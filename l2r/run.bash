b0369e3186ddbe53c00dca2e3f326c07f57e0008
Jimmy Herman <jamesher@andrew.cmu.edu>
2021-03-10 19:58:45 -0500

    added mpc optional arg

--- l2r/run.bash
+++ l2r/run.bash
@@ -12,6 +12,8 @@ if [ "$1" = "random" ]; then
 	python3 scripts/runner_random.py configs/params_random.yaml
 elif [ "$1" = "sac" ]; then
 	python3 scripts/runner_sac.py configs/params_sac.yaml
+elif [ "$1" = "mpc" ]; then
+	python3 scripts/runner_mpc.py configs/params_mpc.yaml
 else
 	python3 scripts/runner.py configs/params.yaml
 fi
