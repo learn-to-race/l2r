e797f9b255c0f4de86588ce59b9ddb918d2b2cc6
Jimmy Herman <jamesher@andrew.cmu.edu>
2021-03-25 16:30:30 -0400

    updated docstrings

--- l2r/racetracks/mapping.py
+++ l2r/racetracks/mapping.py
@@ -17,8 +17,7 @@ def level_2_trackmap(level):
     """Utility to convert a human readable track name to the filepath of
     the racetrack's map.
 
-    :param level: name of the racetrack, must be in [...]
-    :type level: string
+    :param str level: name of the racetrack
     :returns: the filepath of the racetrack's map, random start positions in
       the form [x,y,z,yaw]
     :rtype: string, list of lists
@@ -37,10 +36,8 @@ def level_2_simlevel(level, sim_version):
     """Utility to convert a human readable track name to the name of the track
     used in the simulator (typically a filepath)
 
-    :param level: name of the racetrack, must be in [...]
-    :type level: string
-    :param sim_version: version of the simulator being used
-    :type sim_version: string
+    :param str level: name of the racetrack
+    :param str sim_version: version of the simulator being used
     :returns: the filepath of the racetrack used by the simulator
     :rtype: string
     """
