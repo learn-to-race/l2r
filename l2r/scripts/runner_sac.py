1196efd8e113d3e0cde30cf4efd4384ecd78ff76
Jonathan Francis <jonathanmfrancis@gmail.com>
2021-12-03 01:58:40 -0500

    Updates, Patches, Housekeeping

--- l2r/scripts/runner_sac.py
+++ l2r/scripts/runner_sac.py
@@ -9,30 +9,34 @@ import json, os, sys
 
 from ruamel.yaml import YAML
 from datetime import date, datetime, timezone
+from types import SimpleNamespace
 
 l2r_path = os.path.abspath(os.path.join(''))
 if l2r_path not in sys.path: sys.path.append(l2r_path)
 import ipdb as pdb
 
 from baselines.rl.sac import SACAgent
-from common.utils import setup_logging
+from common.utils import setup_logging, resolve_envvars
 from envs.env import RacingEnv
+
+args = {'runtime': 'local', 'dirhash':''}
+args = SimpleNamespace(**args)
 
 if __name__ == "__main__":
 
     # load configuration file
     yaml = YAML()
     agent_params = yaml.load(open(sys.argv[1]))
-    agent_kwargs = agent_params['agent_kwargs']
+    agent_kwargs = resolve_envvars(agent_params['agent_kwargs'], args)
 
     sys_params = yaml.load(open(f"{sys.argv[1].split('/')[0]}/params-env.yaml"))
-    env_kwargs = sys_params['env_kwargs']
-    sim_kwargs = sys_params['sim_kwargs']
+    env_kwargs = resolve_envvars(sys_params['env_kwargs'], args)
+    sim_kwargs = resolve_envvars(sys_params['sim_kwargs'], args)
 
     # overrrides
     env_kwargs['action_if_kwargs']['max_accel'] = 6.
     env_kwargs['action_if_kwargs']['min_accel'] = -2
-    env_kwargs['eval_mode'] = False
+    env_kwargs['eval_mode'] = True
 
     # create results directory
     save_path = agent_kwargs['model_save_path']
